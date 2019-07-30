0d8195afbbd53b7d84e3a29471bdf12d7849f177
Keloudi the brid <lpapakia@hotmail.com>
2019-07-30 18:36:20 +0300

    Update ika.py

--- ika.py
+++ ika.py
@@ -1,7 +1,10 @@
 from sklearn.neural_network import MLPClassifier
 import pandas as pd
+import numpy as np
 import warnings
 import pickle
+import time
+import sys
 
 
 ############################################ SIMPLE READING/SAVING FUNCTIONS ###########################################
@@ -71,6 +74,82 @@ def find_percentile(df):
     return list_of_dicts, list_of_names
 
 
+def find_lists(array):
+    cities = array['city'].unique()
+    registration = array['registered_via'].unique()
+    del array
+    return cities, registration
+
+
+def find_min_and_max(time, imerominia):
+    min_time = time.min()
+    max_time = time.max()
+    # year month day format
+    year, month, day = date(imerominia)
+    min_year = year.min()
+    max_year = year.max()
+    return min_time, max_time, min_year, max_year
+
+
+def age(array):
+    array = array.where(array < 18, 18)
+    array = array.where(array > 80, 23)
+    return array
+
+
+def date(array):
+    array = array.astype(str)
+
+    year = array.str[0:4]
+    year = year.astype(int)
+
+    month = array.str[4:6]
+    month = month.astype(int)
+
+    day = array.str[6:8]
+    day = day.astype(int)
+    return year, month, day
+
+
+def date_for_members(array, min_year, max_year):
+    year, month, day = date(array)
+    year = (year - min_year)/(max_year-min_year)
+    month = (month - 1)/11
+    day = (day -1)/30
+    return year, month, day
+
+
+def correct(prediction, true_values):
+    check = np.equal(prediction, true_values)
+    check = np.sum(check)
+    check = (check*100)/len(prediction)
+    return check
+
+
+def loading_bar(i):
+    if i == 0:
+        i = '0%'
+    else:
+        i = (i*1000000)//7377418
+        i = (i//4)*'█' + " " + str(i) + "%"
+    time.sleep(1)
+    sys.stdout.write('\r' + i)
+    sys.stdout.flush()
+
+
+def loading_bar_test(i, score):
+    if i == 0:
+        time.sleep(1)
+        sys.stdout.write(' 0%')
+        sys.stdout.flush()
+    else:
+        i = (i*1000000)//2556789
+        i = (i//4)*'█' + " " + str(i) + "%"
+        time.sleep(1)
+        sys.stdout.write('\r' + i + ' While the score is ' + str(score) + " %")
+        sys.stdout.flush()
+
+
 ############################################### MAKES THE SONGS DUMMIES ###############################################
 def make_dummies(array, dict_for_map, names, column_name):
     # NaN values aren't allowed, so they're filled as 'OTHER' (such subcategory exists in every category)
@@ -81,9 +160,10 @@ def make_dummies(array, dict_for_map, names, column_name):
     for column in array:
         array[column] = array[column].map(dict_for_map)
     # Makes dummies
-    array = pd.get_dummies(array.apply(pd.Series).stack(), prefix=column_name).sum(level=0)
+    array = pd.get_dummies(array.apply(pd.Series).stack()).sum(level=0)
     # Creates more columns for dummy variables that are not available
     array = array.T.reindex(names).T.fillna(0)
+    array.columns = [column_name + '_' + str(col) for col in array.columns]
     # Keeps them in alphabetical order, so that the columns aren't in random order each time
     array = array.reindex(sorted(array.columns), axis=1)
     return array
@@ -99,7 +179,11 @@ def manipulate_song(array, maping, listing, max_length):
     array['song_length'] = array['song_length'].div(max_length)
     # Get language dummies (no two languages appear in a single column,
     # so the extra function is not needed for this column)
-    array['language'] = array['language'].T.reindex(listing[4]).T.fillna(0)
+    language = pd.get_dummies(array['language'])
+    language = language.T.reindex(listing[4]).T.fillna(0)
+    language.columns = ['language_' + str(col) for col in language.columns]
+    language = language.reindex(sorted(language.columns), axis=1)
+    array = array.drop(['language'], axis=1)
     column_list = ['genre_ids', 'artist_name', 'composer', 'lyricist']
     i = 0
     # Makes dummies for each "more demanding" column
@@ -108,7 +192,38 @@ def manipulate_song(array, maping, listing, max_length):
         array = pd.concat([array, column], axis=1)
         i = i + 1
     # Drops the original columns, since now they're replaced with the dummy columns
+    array = pd.concat([array, language], axis=1)
     array = array.drop(['genre_ids', 'artist_name', 'composer', 'lyricist'], axis=1)
+    return array
+
+
+def manipulate_member(array, cities, registration, min_time, max_time, min_year, max_year):
+    #['msno', 'city', 'bd', 'gender', 'registered_via', 'registration_init_time', 'expiration_date']
+    city = pd.get_dummies(array['city'])
+    city = city.T.reindex(cities).T.fillna(0)
+    city.columns = ['city_' + str(col) for col in city.columns]
+    city = city.reindex(sorted(city.columns), axis=1)
+
+    registered = pd.get_dummies(array['registered_via'])
+    registered = registered.T.reindex(registration).T.fillna(0)
+    registered.columns = ['registered_via_' + str(col) for col in registered.columns]
+    registered = registered.reindex(sorted(registered.columns), axis=1)
+
+    gender = pd.get_dummies(array['gender'])
+    gender.columns = ['gender_' + str(col) for col in gender.columns]
+    gender = gender.reindex(sorted(gender.columns), axis=1)
+
+    array['bd'] = age(array['bd'])
+
+    array['registration_init_time'] = array['registration_init_time']-min_time/max_time - min_time
+
+    array = array.drop(['city', 'gender', 'registered_via'], axis=1)
+    array = pd.concat([array, city], axis=1)
+    array = pd.concat([array, gender], axis=1)
+    array = pd.concat([array, registered], axis=1)
+
+    array['year'], array['month'], array['day'] = date_for_members(array['expiration_date'], min_year, max_year)
+    array = array.drop(['expiration_date'], axis=1)
     return array
 
 
@@ -122,8 +237,10 @@ def manipulate_train(array, categories):
     # Make the dummies happen
     for i in range(3):
         column = array[relevant_categories[i]]
-        column = pd.get_dummies(column, prefix=relevant_categories[i])
+        column = pd.get_dummies(column)
         column = column.T.reindex(categories[i]).T.fillna(0)
+        column.columns = [relevant_categories[i] + '_' + str(col) for col in column.columns]
+        column = column.reindex(sorted(column.columns), axis=1)
         array = pd.concat([array,column], axis=1)
     # Drop the original columns, like before
     array = array.drop(relevant_categories, axis=1)
@@ -131,34 +248,94 @@ def manipulate_train(array, categories):
 
 
 ###################################################### MERGES THEM #####################################################
-def train_with_songs(train, song):
+def implement(array, song, members):
     # Rather simple. Gets rid of the ids, and then merges them together. Simple as.
     song = song.drop(['song_id'],axis=1)
-    train = train.drop(['msno', 'song_id'],axis=1)
-    train = pd.concat([train,song],axis=1)
-    train = train.fillna(0)
-    return train
+    array = array.drop(['msno', 'song_id'],axis=1)
+    members = members.drop(['msno'], axis=1)
+    array = pd.concat([array,song],axis=1)
+    array = pd.concat([array,members],axis=1)
+    array = array.fillna(0)
+    return array
 
 
 ##################################################### TRAINS THEM #####################################################
-def train_them(train, songs, maping, listing, max_length, categories):
+def train_them(train, members, songs, maping, listing, max_length, categories):
     prev = 0
+    i = 0
     mlp = MLPClassifier(warm_start=True)
+    entire_members = file_read(members)
+    city_list, registration_list = find_lists(entire_members)
+    min_time, max_time, min_year, max_year = find_min_and_max(entire_members['registration_init_time'],
+                                                              entire_members['expiration_date'])
     # Reads in 10k chunks
+    # 400000 einai to max
     while prev + 10000 < 7377418:
+        loading_bar(i)
         # Takes care of the arrays first. Must be encoded, normalized and ready to go
+        new_member = manipulate_member(chunk_read(members, prev, prev + 10000),city_list,registration_list,
+                                       min_time,max_time,min_year,max_year)
         new_song = manipulate_song(chunk_read(songs, prev, prev+10000), maping, listing, max_length)
         new_train, target = manipulate_train(chunk_read(train, prev, prev+10000),categories)
-        new_train = train_with_songs(new_train, new_song)
+        new_train = implement(new_train, new_song, new_member)
         # Feeds them into the algorithm, it's good to go!
         mlp.fit(new_train, target)
         prev = prev + 10000
+        i = i + 1
     # Runs one last time. Same as before, it's just a smaller chunk now.
     new_song = manipulate_song(chunk_read(songs, prev, 7377418), maping, listing, max_length)
     new_train, target = manipulate_train(chunk_read(train, prev, 7377418), categories)
-    new_train = train_with_songs(new_train, new_song)
+    new_train = implement(new_train, new_song)
     mlp.fit(new_train, target)
+    loading_bar(7377418, 7377418)
+    print('\n')
+    test_them(mlp, maping, listing, max_length, categories, city_list, registration_list,
+              min_time, max_time, min_year, max_year)
     # Done with the fitting! Time to put it to the test!
+
+
+def test_them(mlp, maping, listing, max_length, categories, city_list, registration_list,
+              min_time, max_time, min_year, max_year):
+    test_path = '/home/lydia/PycharmProjects/untitled/currently using/test_with_target.h5'
+    test_songs_path = '/home/lydia/PycharmProjects/untitled/currently using/repeated_songs_test.h5'
+    test_members_path = '/home/lydia/PycharmProjects/untitled/currently using/repeated_members_test.h5'
+    prev = 0
+    i = 0
+    while prev+10000 < 2556789:
+        test_members = manipulate_member(chunk_read(test_members_path, prev, prev+10000), city_list, registration_list,
+                                         min_time, max_time, min_year, max_year)
+        test_song = manipulate_song(chunk_read(test_songs_path,prev,prev+10000), maping, listing, max_length)
+        test, target = manipulate_train(chunk_read(test_path,prev,prev+10000), categories)
+        test = implement(test, test_song, test_members)
+        test = test.drop(['id'], axis=1)
+        target = target.reset_index(drop=True)
+        nan_index = target[target.apply(np.isnan)]
+        nan_index = nan_index.index.tolist()
+        target = target.dropna()
+        prediction = mlp.predict(test)
+        prediction = np.delete(prediction, nan_index)
+        score = correct(prediction, target)
+        prediction = pd.DataFrame(prediction)
+        file_save(prediction, 'Score.h5')
+        prev = prev + 10000
+        loading_bar_test(i, score)
+        i = i + 1
+    test_members = manipulate_member(chunk_read(test_members_path, prev, 2556789), city_list, registration_list,
+                                     min_time, max_time, min_year, max_year)
+    test_song = manipulate_song(chunk_read(test_songs_path, prev, 2556789), maping, listing, max_length)
+    test, target = manipulate_train(chunk_read(test_path, prev, 2556789), categories)
+    test = implement(test, test_song, test_members)
+    test = test.drop(['id'], axis=1)
+    target = target.reset_index(drop=True)
+    nan_index = target[target.apply(np.isnan)]
+    nan_index = nan_index.index.tolist()
+    target = target.dropna()
+    prediction = mlp.predict(test)
+    prediction = np.delete(prediction, nan_index)
+    score = correct(prediction, target)
+    prediction = pd.DataFrame(prediction)
+    file_save(prediction, 'Score.h5')
+    loading_bar_test(i, score)
 
 
 
@@ -168,11 +345,13 @@ warnings.filterwarnings('ignore')
 # Simply reads the files
 train_path = '/home/lydia/PycharmProjects/untitled/old uses/train.h5'
 file_path = '/home/lydia/PycharmProjects/untitled/currently using/'
+members_path = file_path + 'repeated_members.h5'
 songs_path = file_path + 'repeated_songs.h5'
 percentile_map = read_list(file_path + 'percentile_map.txt')
 categories = read_list(file_path + 'categories.txt')
 max_song_length = read_list(file_path + 'max_song_length.txt')
 percentile_list = read_list(file_path + 'percentile_list.txt')
 
+
 # All read and good. Ready to go!
-train_them(train_path, songs_path, percentile_map, percentile_list, max_song_length, categories)
+train_them(train_path, members_path, songs_path, percentile_map, percentile_list, max_song_length, categories)
